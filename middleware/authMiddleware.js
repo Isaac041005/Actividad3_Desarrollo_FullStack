f0377ab2dca22e130532fda8f82a9c146a922593
Isaac041005 <al07003035@tecmilenio.mx>
2025-02-12 12:49:32 -0600

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,20 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'supersecreto123';
 
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Acceso denegado, token requerido' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Token inválido' });
+    }
+};
+
+module.exports = authMiddleware;
