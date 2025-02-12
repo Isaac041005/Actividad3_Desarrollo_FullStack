310905b046d1938895e2c3b65566997538ee8250
Isaac041005 <al07003035@tecmilenio.mx>
2025-02-12 12:45:47 -0600

    Update tareas.js

--- routes/tareas.js
+++ routes/tareas.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const { getTareas, createTarea, updateTarea, deleteTarea } = require('../controllers/tareasController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Rutas protegidas por autenticación
+router.get('/', authMiddleware, getTareas);
+router.post('/', authMiddleware, createTarea);
+router.put('/:id', authMiddleware, updateTarea);
+router.delete('/:id', authMiddleware, deleteTarea);
+
+module.exports = router;
 
